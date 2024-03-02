029057183c53311aa225d8f327dc906baa7df844
anim200 <animislam2000@gmail.com>
2024-03-02 15:54:00 +0600

    c

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -44,7 +44,7 @@ export default function Hero() {
     <div className="hero">
       <div className="wrapper">
       <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
-            <motion.h2 variants={textVariants}>Miftahul Islam</motion.h2>
+            <motion.h2 variants={textVariants}>Miftahl Islam</motion.h2>
             <motion.h1 variants={textVariants}>Web Developer And UI DESIGNER</motion.h1>
             <motion.div variants={textVariants} className="buttons">
                 <motion.button variants={textVariants}>See the Latest Works</motion.button>
